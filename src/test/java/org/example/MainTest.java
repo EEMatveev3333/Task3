1e5ab77669be390bf951d4f49e5a649d1381c463
EEMatveev <eematveev333@gmail.com>
2024-03-20 15:23:44 +0300

    Commit20

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -24,7 +24,32 @@ class MainTest {
     }
 
     @org.junit.jupiter.api.Test
-    @DisplayName("Тест 2. Проверить чтение из кэша искуственно скорректированным кэшированное значением.")
+    @DisplayName("Тест 2. Проверить запись в кэш нескольких значений")
+    void testUncachedValue() {
+        Fraction fr = new Fraction(2, 3);
+        System.out.println("===!");
+        Fractionable num = Utils.cache(fr);
+        System.out.println("            " + String.valueOf(num.doubleValue()));// sout сработал
+        assertNotEquals(Utils.referPersonInvocationHandler.godHashMap.size(), 1);
+
+        num.setNum(5);
+        System.out.println("            " + String.valueOf(num.doubleValue()));// sout сработал
+        assertNotEquals(Utils.referPersonInvocationHandler.godHashMap.size(), 1);
+
+        num.setDenum(15);
+        System.out.println("            " + String.valueOf(num.doubleValue()));// sout сработал
+        assertNotEquals(Utils.referPersonInvocationHandler.godHashMap.size(), 1);
+
+        num.setNum(20);
+        num.setDenum(40);
+        System.out.println("            " + String.valueOf(num.doubleValue()));// sout сработал
+        assertNotEquals(Utils.referPersonInvocationHandler.godHashMap.size(), 1);
+
+        System.out.println("===!");
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Тест 3. Проверить чтение из кэша искуственно скорректированным кэшированное значением.")
     void testCrushedCachedValue() {
         Fraction fr = new Fraction(2, 3);
         System.out.println("===!");
@@ -34,8 +59,10 @@ class MainTest {
         assertEquals(String.valueOf(num.doubleValue()), String.valueOf(2.0 / 3.0));
         //        Искуствено меняем содержимое кэша на другое число
         double test_val = 0.25;
-        Utils.referPersonInvocationHandler.ObjectsCache.put("doubleValue", (Object) test_val);
-        assertNotEquals(String.valueOf(num.doubleValue()), String.valueOf(test_val));
+        //Utils.referPersonInvocationHandler.ObjectsCache.put("doubleValue", (Object) test_val);
+        //Utils.referPersonInvocationHandler.godHashMap.entrySet().stream().distinct()
+
+        assertEquals(String.valueOf(num.doubleValue()), String.valueOf(test_val));
 
         System.out.println("            " + String.valueOf(num.doubleValue()));// sout молчит
         num.setNum(5);
@@ -47,7 +74,7 @@ class MainTest {
     }
 
     @org.junit.jupiter.api.Test
-    @DisplayName("Тест 3. Проверить очистку кэша по таймеру через чтение из кэша искуственно скорректированным кэшированное значением и последующего его удаления")
+    @DisplayName("Тест 4. Проверить очистку кэша по таймеру через чтение из кэша искуственно скорректированным кэшированное значением и последующего его удаления")
     void testCleanedCachedValue() throws InterruptedException {
         Fraction fr = new Fraction(2, 3);
         System.out.println("===!");
